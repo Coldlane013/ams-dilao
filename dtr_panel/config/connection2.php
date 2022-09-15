078acde073c42f711149a27b98e07dce32bf805b
Laine <75091424+Coldlane013@users.noreply.github.com>
2022-09-15 22:36:20 +0800

    Delete connection2.php

--- dtr_panel/config/connection2.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$conn = new mysqli("localhost", "root", "", "dtr_system");
-	if($conn->connect_error) {
-	  die('Error connecting to database'); //Should be a message for normal peeps
-	}
-
-?>
